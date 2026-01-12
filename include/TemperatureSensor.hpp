3a6b76bc8b9c9c66494bd858483b3687abfda0f8
1Filipek2 <rybakovfilip12@gmail.com>
2026-01-12 20:24:45 +0100

    feat: complete TemperatureSensor header with correct overrides

---
+++ include/TemperatureSensor.hpp
@@ -0,0 +1,24 @@
+#ifndef TEMPERATURE_SENSOR_HPP
+#define TEMPERATURE_SENSOR_HPP
+
+#include "ISensor.hpp"
+#include <string>
+#include <random>
+
+namespace industrial {
+    
+class TemperatureSensor : public ISensor{
+    private:
+        std::string name;
+        std::mt19937 gen;
+        std::uniform_real_distribution<double> dist;
+    public:
+        TemperatureSensor(std::string name, double min, double max){}
+
+        double getValue() override;
+        std::string getName() const override;
+
+    };
+}
+
+#endif
