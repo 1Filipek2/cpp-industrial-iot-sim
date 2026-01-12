e4969e84b0b59506dbfb14761f43be0fa6a99a85
1Filipek2 <rybakovfilip12@gmail.com>
2026-01-13 00:03:12 +0100

    feat: implement real-time loop with 1s refresh and clear screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <memory>
 #include <iomanip>
+#include <thread>
+#include <chrono>
 
 #include "TemperatureSensor.hpp"
 #include "PressureSensor.hpp"
@@ -9,34 +11,44 @@
 int main () {
     std::vector<std::unique_ptr<industrial::ISensor>> sensors;
 
-    try
+    sensors.push_back(std::make_unique<industrial::TemperatureSensor>("Main Boiler", 20.0, 100.0));
+    sensors.push_back(std::make_unique<industrial::PressureSensor>("Hydraulic Pump", 0.0, 10.0));
+
+
+    while (true)
     {
-        sensors.push_back(std::make_unique<industrial::TemperatureSensor>("Main Boiler", 20.0, 100.0));
-        sensors.push_back(std::make_unique<industrial::PressureSensor>("Hydraulic Pump", 0.0, 10.0));
+        std::cout << "\033[2J\033[1;1H";
+        std::cout << "--- Industrial IoT Monitor (Ctrl+C to stop) ---" << std::endl << std::endl;
 
-        for(const auto& sensor : sensors) {
-            double val = sensor->getValue();
+        try
+        {
+            for(const auto& sensor : sensors) {
 
-            bool unsafe = !sensor->isSafe();
+                double val = sensor->getValue();
+                bool unsafe = !sensor->isSafe();
 
-            if(unsafe) {
-                std::cout << "\033[1;31m[ALARM] ";
+                if(unsafe) {
+                    std::cout << "\033[1;31m[ALARM] ";
+                }
+
+                std::cout << "Sensor: " << std::left << std::setw(15) << sensor->getName() 
+                        << " | Reading: " << std::fixed << std::setprecision(2) 
+                        << std::right << std::setw(6) << val 
+                        << " " << sensor->getUnit();
+
+                if(unsafe) {
+                    std::cout << "\033[0m";
+                }
+                std::cout << std::endl;
             }
 
-            std::cout << "Sensor: " << std::left << std::setw(15) << sensor->getName() 
-                      << " | Reading: " << std::fixed << std::setprecision(2) 
-                      << std::right << std::setw(6) << val 
-                      << " " << sensor->getUnit();
-
-            if(unsafe) {
-                std::cout << "\033[0m";
-            }
-            std::cout << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
         }
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
+        catch(const std::exception& e)
+        {
+            std::cerr << "Critical error: " << e.what() << std::endl;
+            break;
+        }
     }
     
     return 0;
